e6a3722b3511c8f97f115924f5e3c74c28f7e6de
natureloved <akinolaa769@gmail.com>
2026-03-18 10:11:56 +0100

    Update syntax for SPDX and compiler versions

--- submissions/Adejoke/day-20/FortKnox.sol
+++ submissions/Adejoke/day-20/FortKnox.sol
@@ -1,5 +1,6 @@
+
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.34;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
--- submissions/Adejoke/day-21/SimpleNFT.sol
+++ submissions/Adejoke/day-21/SimpleNFT.sol
@@ -1,5 +1,6 @@
+
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.34;
 
 interface IERC721 {
     event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
